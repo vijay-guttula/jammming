02e09cf28402fdd7a2a21b1f794bf44a66462736
Vijay Guttula <ssvijayg@gmail.com>
2021-05-11 18:01:05 +0530

    Obtain spotify access token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,11 +1,31 @@
-const userAccessToken = '';
+let accessToken;
+const clientID = ' 76e20c70134a432086ba29c82c1b181b';
+const redirectURI = 'http://localhost:3000/';
 
-const Spotify = () =>{
- getAccessToken(){
-  if (userAccessToken.length !== 0){
-   return userAccessToken;
-  } 
- }
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    // checking for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    // if accesstoken and expiration time are in the url
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      // clearing parameters from url
+      window.setTimeout(() => (accessToken = ''), expiresInMatch * 1000);
+      window.history.pushState('Access Token', null, '/');
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+
+      window.location = accessUrl;
+    }
+  },
 };
 
 export default Spotify;
